d9eb678d04d7c38054001246811afcfee614c8bb
NathanRouille <nathan.rouille@yahoo.fr>
2024-02-06 21:28:24 +0100

    fusion 2 programmes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import lzma
 import sys
 from PIL import Image
 from io import BytesIO
@@ -23,13 +24,38 @@ def getCollection(collection_name):
             r_img = requests.get(nft['token']['image'], stream = True)
             match = re.search(r'#(\d+)', nft['token']['name'])
             if match and r_img.status_code == 200:
-                id = match.group(1)
+                id = int(match.group(1))
                 image = Image.open(BytesIO(r_img.content))
                 prix = nft['price']
                 dict[id] = (image,prix)
 
     return dict
 
+
+def ncd(x,y):
+    x_y = x + y  # the concatenation of files
+
+    x_comp = lzma.compress(x)  # compress file 1
+    y_comp = lzma.compress(y)  # compress file 2
+    x_y_comp = lzma.compress(x_y)  # compress file concatenated
+
+    # print len() of each file
+    print(len(x_comp), len(y_comp), len(x_y_comp), sep=' ', end='\n')
+
+    # magic happens here
+    ncd = (len(x_y_comp) - min(len(x_comp), len(y_comp))) / max(len(x_comp), len(y_comp))
+
+    return ncd
+
 if __name__ == '__main__':
     collection_name = sys.argv[1]
+    nft_id = int(sys.argv[2])
     collection_dict = getCollection(collection_name)
+    print(co)
+    dp=[]
+    for id in collection_dict:
+        if id != nft_id:
+            dp.append((1-ncd(collection_dict[id][0],collection_dict[nft_id][0]),collection_dict[id][1]))
+
+    price=sum([d * p for d, p in dp])/sum([d for d,_  in dp])
+    print(price)
