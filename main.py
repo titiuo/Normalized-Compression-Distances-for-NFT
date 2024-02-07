b0674105688f979d50695f0c6bc7d63e6df0fefa
Titouan <43248346+titiuo@users.noreply.github.com>
2024-02-07 15:25:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,11 @@ def getCollection(collection_name):
 
     url = f"https://api-mainnet.magiceden.dev/v2/collections/{collection_name}/listings"
     url_stats = f"https://api-mainnet.magiceden.dev/v2/collections/{collection_name}/stats"
-    url_attributs = f"https://api-mainnet.magiceden.dev/v2/collections/{collection_name}/attributes"
 
     headers = {"accept": "application/json"}
 
     response_stat = requests.get(url_stats, headers=headers)
     listed_count = int(response_stat.json()['listedCount'])
-
-    listed_count = 100
 
     for i in range(listed_count//100): #modifier le pas et filtrer les prix trop hauts / prendre en compte les ventes
         params = {"limit": 100,"offset":100*i}
@@ -33,14 +30,17 @@ def getCollection(collection_name):
 
     return dict
 
+def compress(x):
+    return lzma.compress(x)
 
 def ncd(x,y):
     x_y = x + y  # the concatenation of files
 
-    x_comp = lzma.compress(x)
-    y_comp = lzma.compress(y)
-    x_y_comp = lzma.compress(x_y)
+    x_comp = lzma.compress(x)  # compress file 1
+    y_comp = lzma.compress(y)  # compress file 2
+    x_y_comp = lzma.compress(x_y)  # compress file concatenated
 
+    # magic happens here
     ncd = (len(x_y_comp) - min(len(x_comp), len(y_comp))) / max(len(x_comp), len(y_comp))
 
     return ncd
@@ -57,7 +57,6 @@ def see_in_dict(collection_name,id):
 
 
 if __name__ == '__main__':
-    
     collection_name = sys.argv[1]
     nft_id = int(sys.argv[2])
     if sys.argv[3]=="y":
@@ -66,15 +65,22 @@ if __name__ == '__main__':
         print("ok")
     else:
         collection_dict = getCollection(collection_name)
-        #collection_dict_str={str(key): value for key,value in collection_dict.items()}
         with open(collection_name,'wb') as f:
             pickle.dump(collection_dict,f)
             print("ok")
     dp=[]
+    i=1
+    pas=5
+    L=[pas*i for i in range(100) if pas*i<=100]
+    a=0
+    n=len_dict(collection_name)
     for id in collection_dict:
-        if id != nft_id:
+        if collection_dict[id][1] < 1000:
             dp.append((1-ncd(collection_dict[id][0],collection_dict[nft_id][0]),collection_dict[id][1])) #si nft_id pas dans la liste l'ajouter
-            print(id)
+            percent=i*100/n
+            if percent>L[a]:
+                print(f"{round(percent)}%")
+                a+=1
+            i+=1
     price=sum([d * p for d, p in dp])/sum([d for d,_  in dp])
     print(f"Prix estimé : {price}\nPrix réelle : {see_in_dict(collection_name,nft_id)[1]}")
-    
